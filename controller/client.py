01cf442a4f8b72317b4ca6e67cdbc78268a67be3
hugo <99veryverygood@gmail.com>
2019-05-12 16:16:57 +0800

    socker 更新

--- controller/client.py
+++ controller/client.py
@@ -1,15 +1,18 @@
 import socket               # 导入 socket 模块
 
-Port = 9006					# 設定的 Port ，與 run.bat 同步
 
-s = socket.socket()         # 建立 socket 物件
-host = socket.gethostname() # 獲得電腦主機名稱
+def connect():
+	Port = 9006					# 設定的 Port ，與 run.bat 同步
 
-s.connect((host, Port))     # 連接到 9006 Port
+	s = socket.socket()         # 建立 socket 物件
+	host = socket.gethostname() # 獲得電腦主機名稱
+	
+	s.connect((host, Port))     # 連接到 9006 Port
+	return s
+
 
 
 msg = input("open file > ")
-msg = './controller/image/' + msg
 s.send( msg.encode('utf-8') )
 
 s.close()
