06cb2460f3e39ac0e49d24387aea944355e81098
hugo <99veryverygood@gmail.com>
2019-05-21 19:19:25 +0800

    相機及文件開發完成

--- controller/PTZ.py
+++ controller/PTZ.py
@@ -19,15 +19,15 @@ class PTZ:
 
 
     class Camera:
-        """ PTZ 的相機 """
-        imageDir = './image/'
-        fileName = 'output_'
-        fileCounter = 0
+        """PTZ 的相機 """
+        _imageDir = './image/'
+        _fileName = 'output_'
+        _fileCounter = 0
         _isVideo = True
         _isCapture = False
 
         def __init__(self, cameraId=0):
-            """ 連接相機，出錯會回傳 false """
+            """連接相機，出錯會回傳 false """
             try:
                 self.cap = cv2.VideoCapture( cameraId )
             except:
@@ -35,18 +35,18 @@ class PTZ:
                 return None
 
         def __del__(self):
-            # 關閉相機及所有 OpenCV 視窗
+            """關閉相機及所有 OpenCV 視窗 """
             self.cap.release()
             cv2.destroyAllWindows()
 
         def showVideo( self ):
-            """ 開執行序顯示畫面 """
+            """開執行序顯示畫面 """
             self._isVideo = True
             t = threading.Thread(target = self._Camera__videoThread )
             t.start()
 
         def __videoThread( self ):
-            """ 顯示畫面的執行序，用 isVideo 及 isCapture 控制 """
+            """顯示畫面的執行序，用 isVideo 及 isCapture 控制 """
             while self._isVideo:
                 ret , frame = self.cap.read()
                 if not ret:
@@ -56,8 +56,8 @@ class PTZ:
                 cv2.imshow('frame', frame )
 
                 if self._isCapture: # 存檔
-                    name = '%s%s%03d.jpg' % ( self.imageDir , self.fileName , self.fileCounter )
-                    self.fileCounter += 1
+                    name = '%s%s%03d.jpg' % ( self._imageDir , self._fileName , self._fileCounter )
+                    self._fileCounter += 1
                     cv2.imwrite( name  , frame )
                     self._isCapture = False
 
@@ -68,27 +68,32 @@ class PTZ:
                     sys.exit()
 
                 if wait & 0xFF == ord('k'):
-                    name = '%s%s%03d.jpg' % ( self.imageDir , self.fileName , self.fileCounter )
-                    self.fileCounter += 1
+                    name = '%s%s%03d.jpg' % ( self._imageDir , self._fileName , self._fileCounter )
+                    self._fileCounter += 1
                     cv2.imwrite( name  , frame )
                     print( 'type k and save an image... %s' % name )
 
         def getFrame( self ):
+            """由外部呼叫，會回傳存圖的名稱 """
             self._isCapture = True
             while self._isCapture:
                 pass
-            name = '%s%03d.jpg' % (self.fileName, self.fileCounter-1 )
+            name = '%s%03d.jpg' % (self._fileName, self._fileCounter-1 )
             return name
+
+        def help( self ):
+            """獲得文件說明 """
+            d = PTZ.Camera.__dict__
+            for k in d.keys():
+                if( str( type( d[k] ) ) == "<class 'function'>" ):
+                    print(  k  , '\n\t' , d[k].__doc__ )
 
     class Controller:
         pass
 
 
 if __name__ == '__main__':
+    # ca.help()
     ca = PTZ.Camera()
     ca.showVideo()
-
-
-    while True:
-        time.sleep(3)
-        print( ca.getFrame() )
+    
